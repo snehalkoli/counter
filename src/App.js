e63a5e3d1ceefa6392a4f3334d2bb4abefc42e73
snehalkoli <67510836+snehalkoli@users.noreply.github.com>
2022-10-30 21:10:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import { useState } from "react";
+function ButtonIncrement(props) {
+  return (
+    <button style={{ marginLeft: ".5rem" }} onClick={props.onClickFunc}>
+      +1
+    </button>
+  );
+}
+function ButtonDecrement(props) {
+  return (
+    <button style={{ marginLeft: ".5rem" }} onClick={props.onClickFunc}>
+      -1
+    </button>
+  );
+}
+function Display(props) {
+  return <label style={{ marginLeft: ".5rem" }}>{props.message}</label>;
+}
+export default function App() {
+  const [counter, setCounter] = useState(1);
+  const incrementCounter = () => setCounter(counter + 1);
+  let decrementCounter = () => setCounter(counter - 1);
+  if (counter <= 1) {
+    decrementCounter = () => setCounter(1);
+  }
+  return (
+    <div>
+      <ButtonIncrement onClickFunc={incrementCounter} />
+      <Display message={counter} />
+      <ButtonDecrement onClickFunc={decrementCounter} />
+    </div>
+  );
+}
